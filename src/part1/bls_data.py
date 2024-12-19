a2bce15fd7b99b42abf185b934a0c5f235d39856
Mani Perumal <maniganda.srinivasan@yassir.com>
2024-12-19 11:17:27 +0530

    Added Part-3 as .pynb file

--- src/part1/bls_data.py
+++ src/part1/bls_data.py
@@ -3,25 +3,52 @@ import requests
 import hashlib
 import os
 from botocore.exceptions import ClientError
+from bs4 import BeautifulSoup
 
 # Constants for S3 bucket and file details
 BUCKET_NAME = 'rearc-assessment'
-S3_FILE_KEY = 'bls/pr.data.0.Current'  # Path of the file in the S3 bucket
 BASE_URL = "https://download.bls.gov/pub/time.series/pr/" # Base URL of the BLS website
-WEBSITE_URL = BASE_URL + "pr.data.0.Current"  # Complete URL of the BLS website
 USER_AGENT = "MyLambdaApp/1.0 (contact@example.com)"  # Added User-Agent to comply with BLS data policy
 
 # S3 client
 s3_client = boto3.client('s3')
 
-def fetch_file_from_website(url):
+def extract_file_names_from_website(url):
+    """
+    Wrangling the website data and return the file names 
+    from the HREF links
+    """
+    try:
+
+        # Fetch the HTML content of the webpage
+        headers = {"User-Agent": USER_AGENT}
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx)
+        
+        # Parse the HTML content using BeautifulSoup
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        # Find all 'a' tags and extract their 'href' attributes
+        href_links = [a['href'].split("/")[-1:][0] for a in soup.find_all('a', href=True)]
+        
+        return href_links
+    
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching URL: {e}")
+        return []
+    except Exception as e:
+        print(f"Error parsing HTML: {e}")
+        return []
+
+
+def fetch_file_from_website(url, file_name):
     """
     Fetch the file content from the given URL using an HTTP GET request.
     """
     headers = {"User-Agent": USER_AGENT}
     response = requests.get(url, headers=headers)
     response.raise_for_status()  # Raise exception for HTTP errors
-    print("Successfully fetched file from the website.")
+    print(f"Successfully fetched file: {file_name} from the website.")
     return response.text
 
 
@@ -70,19 +97,28 @@ def sync_data():
     if a change is identifed in the Source website file.
     """
     try:
-        # Fetch the file from the website
-        new_file_content = fetch_file_from_website(WEBSITE_URL)
+        #Extract file names from the website
+        file_names = extract_file_names_from_website(BASE_URL)  
 
-        # Check if the file exists in S3
-        existing_file_content = get_file_from_s3(BUCKET_NAME, S3_FILE_KEY)
+        # Loop the file names to detect the changes and sync with S3 bucket
+        for file_name in file_names:
+            print(f"Started syncing the data of a file::{file_name}")
+            WEBSITE_URL = BASE_URL + file_name  # Complete URL of the BLS website
+            S3_FILE_KEY = 'bls/' + file_name  # Path of the file in the S3 bucket
 
-        # Compare the content using hash values
-        if file_has_changed(existing_file_content, new_file_content):
-            print("File content has changed. Updating S3...")
-            upload_file_to_s3(new_file_content, BUCKET_NAME, S3_FILE_KEY)
-            print("S3 file updated successfully.")
-        else:
-            print("No changes detected. S3 file is up-to-date.")
+            # Fetch the file from the website
+            new_file_content = fetch_file_from_website(WEBSITE_URL, file_name)
+
+            # Check if the file exists in S3
+            existing_file_content = get_file_from_s3(BUCKET_NAME, S3_FILE_KEY)
+
+            # Compare the content using hash values
+            if file_has_changed(existing_file_content, new_file_content):
+                print("File content has changed. Updating S3...")
+                upload_file_to_s3(new_file_content, BUCKET_NAME, S3_FILE_KEY)
+                print("S3 file updated successfully.")
+            else:
+                print("No changes detected. S3 file is up-to-date.")
 
     except Exception as e:
         print(f"An error occurred: {e}")
